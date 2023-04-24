57f9241a76910708af21cc59867ab1a88b8e5e4f
njdam <jeandamngaboyimana@gmail.com>
2023-04-24 05:48:55 -0700

    Updated filenames for "other_f-specs.c" to "100-noncustom_sp.c" and "write_handler.c" to "4-write.c".

--- main.h
+++ main.h
@@ -58,7 +58,7 @@ int print_int(va_list arg, char buffer[], int flags, int width,
 int print_binary(va_list arg, char buffer[], int flags, int width,
 		int precision, int size);
 
-/**** Qu.3: Conversion specifiers ****/
+/**** Qu.3: cunstom Conversion specifiers ****/
 int print_uns(va_list arg, char buffer[], int flags, int width,
 		int precision, int size);
 int print_octal(va_list arg, char buffer[], int flags, int width,
@@ -96,13 +96,13 @@ int print_pointer(va_list arg, char buffer[], int flags, int width,
 int print_non_printable(va_list arg, char buffer[], int flags,
 		int width, int precision, int size);
 
-/******** Qu.7 - Qu.12: non-custom conversion specifiers ********/
+/******** Qu.7 - Qu.12: 100 - non-custom conversion specifiers ********/
 int check_flags(const char *format, int *x);
 int check_width(const char *format, int *x, va_list arg);
 int check_precision(const char *format, int *x, va_list arg);
 int check_size(const char *format, int *x);
 
-/********* Qu.13-Qu.14: prototype to printing reverse or tr rot13 ********/
+/********* Qu.13-Qu.14: 101 - prototype to printing reverse or tr rot13 ********/
 int print_rev(va_list arg, char buffer[], int flags, int width,
 		int precision, int size);
 int print_rot13(va_list arg, char buffer[], int flags, int width,
