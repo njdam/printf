4ec78bdf380b511f5ace8f5d1bd4c1f09dad210b
Lionel <ndabagajeanlionel@gmail.com>
2023-04-26 00:27:02 +0000

    Flags fixed

--- 100-noncustom_sp.c
+++ 100-noncustom_sp.c
@@ -19,7 +19,7 @@ int check_flags(const char *format, int *x)
 		y = 0;
 		while (F_CH[y] != '\0')
 		{
-			if (format[y] == F_CH[y])
+			if (format[z] == F_CH[y])
 			{
 				flags |= F_AR[y];
 				break;
